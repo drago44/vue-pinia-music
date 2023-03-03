eb4f10f580b93a6812d71715f8111f32d108969e
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-03 17:03:50 +0200

    delete firebase id in firibase.js

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,11 +4,11 @@ import 'firebase/firestore';
 import 'firebase/storage';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAJDQHNCCji1rTV75ugIv6JmJ0f6ig91XY',
-  authDomain: 'vue-pinia-music.firebaseapp.com',
-  projectId: 'vue-pinia-music',
-  storageBucket: 'vue-pinia-music.appspot.com',
-  appId: '1:174942943254:web:d8e7c61def55f664015d37',
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  appId: ,
 };
 
 export default firebase.initializeApp(firebaseConfig);
