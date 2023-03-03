a7247bc4f128f49037d58731ddb1ccb263732175
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-03 17:07:51 +0200

    create .env

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,11 +4,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  projectId: ,
-  storageBucket: ,
-  appId: ,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_PROJECT_ID + '.firebaseapp.com',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_PROJECT_ID + '.appspot.com',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
